07ae1f345e60cf2f8f0120586f61caf3d174e377
timwiesner <timothyjwiesner@gmail.com>
2017-11-20 20:15:48 -0600

    feat: Add show categories route

--- application.py
+++ application.py
@@ -15,9 +15,15 @@ app = Flask(__name__)
 
 # Home route
 @app.route('/')
-@app.route('/inventory')
-def showInventory():
-    return "Hello, world!"
+@app.route('/category')
+def showCategories():
+    return "Show categories"
+
+
+# Edit category
+@app.route('/category/new')
+def newCategory():
+    return "New category"
 
 
 if __name__ == '__main__':
